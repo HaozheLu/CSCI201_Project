23b37081ef2a9ce2f210330dc46361552a4cfd11
Anish Jayant <jayant@usc.edu>
2024-04-22 17:20:34 -0700

    added assignment4 versions for login/signup

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,86 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignupServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+       
+    private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("loginUsername");
+		String password = request.getParameter("loginPassword");
+				
+        String SQLusername = "";
+        String SQLpassword = "";
+        
+        
+        Connection conn = null;
+		Statement st = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			// Class.forName() may only bee needed with old JVMs, like JVM 14
+			// In that case, also remove throws ClassNotFoundException
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String myString = "jdbc:mysql://localhost/Assignment4?user=root";
+			conn = DriverManager.getConnection(myString);
+			st = conn.createStatement();
+			PrintWriter out = response.getWriter();
+			// rs = st.executeQuery("SELECT * from Student where fname='" + name + "'");
+			/* Start Query help form chatgpt */
+		    String sql = "SELECT * FROM Users WHERE uname = ? AND pword = ?";
+		    ps = conn.prepareStatement(sql);
+		    ps.setString(1, username);
+		    ps.setString(2, password);
+            rs = ps.executeQuery();
+            /* End Query help form chatgpt */
+
+            if (rs.next()) // login capable
+            {
+                response.sendRedirect("index.html?username=" + username);		// send us to the login page now :)
+            }
+            else
+            {
+            	// No matching user found
+            	response.sendRedirect("login.html?error=UserNotFound"); // Redirect to login.html
+            }
+		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally { }
+        
+        
+
+	}
+
+}
---
+++ src/main/java/SignupServlet.java
@@ -0,0 +1,84 @@
+package servlet;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignupServlet
+ */
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet {
+       
+    private static final long serialVersionUID = 1L;
+
+	public SignupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("signupUsername");
+		String password = request.getParameter("signupPassword");
+		String confirmPassword = request.getParameter("signupConfirmPassword");
+		String email = request.getParameter("signupEmail");
+
+        if (!confirmPassword.equals(password))
+        {
+            response.sendRedirect("login.html?error=PasswordMismatch");		// send us to the login page now :)
+            return;
+        }
+        // Enter your own username/password here (or configure mySQL Workbench to not require it)
+        String SQLusername = "";        
+        String SQLpassword = "";
+        
+        Connection conn = null;
+		Statement st = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			// Class.forName() may only bee needed with old JVMs, like JVM 14
+			// In that case, also remove throws ClassNotFoundException
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String myString = "jdbc:mysql://localhost/CSCI201_Project?user=root";
+			conn = DriverManager.getConnection(myString);
+			st = conn.createStatement();
+			// rs = st.executeQuery("SELECT * from Student where fname='" + name + "'");
+			/* Start Query help form chatgpt */
+			String sql = "INSERT INTO Users (uname, pword, email) VALUES (?, ?, ?, ?)";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, email);
+            System.out.println("User created " + ps.executeUpdate());
+            response.sendRedirect("login.html?username=" + username);		// send us to the login page now :)
+		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally { }
+	}
+
+}
