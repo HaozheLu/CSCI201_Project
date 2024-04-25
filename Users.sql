c9b765d1dc10b9367078fe5eb206358cbfcc819e
Anish Jayant <jayant@usc.edu>
2024-04-24 22:26:42 -0700

    Update Users.sql

--- Users.sql
+++ Users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `CSCI201_Project`.`Users` (
-  `idUsers` INT NOT NULL,
+  `idUsers` INT NOT NULL AUTO_INCREMENT,
   `uname` VARCHAR(45) NULL,
   `pword` VARCHAR(45) NULL,
   `email` VARCHAR(45) NULL,
